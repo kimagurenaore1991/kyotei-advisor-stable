2c45298210282020b435e8392805687932f81d95
kimagurenaore1991 <152239218+kimagurenaore1991@users.noreply.github.com>
2026-06-06 19:14:07 +0900

    Delete scratch8.py

--- scratch8.py
+++
@@ -1,5 +0,0 @@
-﻿with open('static/index.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for i, line in enumerate(lines):
-    if ' overlay ' in line or 'overlay=' in line or 'const overlay' in line:
-        print(f'Line {i+1}: {line.strip()}')
