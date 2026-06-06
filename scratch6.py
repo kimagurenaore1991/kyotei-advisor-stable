b45ab590f77478a7ecde43681b0f95f0464e499f
kimagurenaore1991 <152239218+kimagurenaore1991@users.noreply.github.com>
2026-06-06 19:13:43 +0900

    Delete scratch6.py

--- scratch6.py
+++
@@ -1,9 +0,0 @@
-﻿with open('static/index.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for i, line in enumerate(lines):
-    if '<script>' in line and i > 500:
-        print(f'Script starts at {i+1}')
-        # print 5 lines before it
-        for j in range(i-5, i):
-            print(f'{j+1}: {lines[j].strip()}')
-        break
