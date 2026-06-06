abe53a32af38f2e9609e4fa5a3d00fd377788ca6
kimagurenaore1991 <152239218+kimagurenaore1991@users.noreply.github.com>
2026-06-06 19:13:19 +0900

    Delete scratch16.py

--- scratch16.py
+++
@@ -1,5 +0,0 @@
-﻿with open('static/index.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for i, line in enumerate(lines):
-    if 'prefetchAdjacentDates(' in line:
-        print(f"Line {i+1}: {line.strip()}")
